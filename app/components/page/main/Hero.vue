545e0669b40629654d48ca685efb77a2a3fa2b6e
admin <admin@admins-MacBook-Air.local>
2026-01-26 18:20:27 +0200

    text start

--- app/components/page/main/Hero.vue
+++ app/components/page/main/Hero.vue
@@ -1,5 +1,25 @@
 <template>
-  <section class="hero">1</section>
+  <section class="hero">
+    <h1 class="h1">H1</h1>
+    <h2 class="h2">H2</h2>
+    <h3 class="h3">H3</h3>
+    <h4 class="h4">H4</h4>
+    <h5 class="h5">H5</h5>
+
+    <p class="sub-l">Subheading Large</p>
+    <p class="sub-m">Subheading Medium</p>
+    <p class="sub-s">Subheading Small</p>
+
+    <p class="body-xl">Body Extra Large</p>
+    <p class="body-l">Body Large</p>
+    <p class="body-m">Body Medium</p>
+    <p class="body-s">Body Small</p>
+
+    <p class="cap-m">Caption Medium</p>
+    <p class="cap-s">Caption Small</p>
+
+    <p class="cta">CTA Standart</p>
+  </section>
 </template>
 
 <script setup lang="ts"></script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="main-page">
-    <CommonHeader />
+    <!-- <CommonHeader /> -->
     <!-- <CommonMenu /> -->
 
     <PageMainHero />
