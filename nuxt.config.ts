60c1a133cbcd6e971987960cba1e45a1544e5b4f
admin <admin@admins-MacBook-Air.local>
2026-02-06 15:20:05 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,34 +46,57 @@ export default defineNuxtConfig({
     pageTransition: { name: "page", mode: "out-in" },
     head: {
       charset: "utf-8",
-      title: "Project Title",
+      title: "Duck",
 
       htmlAttrs: {
         lang: "en",
       },
 
       meta: [
+        // Base
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "theme-color", content: "#0e0e0e" },
+        { charset: "utf-8" },
 
-        // SEO / Social
-        { name: "twitter:card", content: "summary_large_image" },
-        { property: "og:image:width", content: "600" },
-        { property: "og:image:height", content: "314" },
-        { property: "og:image:type", content: "image/jpeg" },
-        { property: "og:site_name", content: "Project Title" },
+        // SEO
+        { name: "title", content: "Duck" },
+        {
+          name: "description",
+          content:
+            "The first meme coin launchpad with contract-level rug protection.No trust required – just code that actually works.",
+        },
+
+        // Open Graph
+        { property: "og:title", content: "Duck" },
+        {
+          property: "og:description",
+          content:
+            "The first meme coin launchpad with contract-level rug protection. No trust required – just code that actually works.",
+        },
         { property: "og:type", content: "website" },
+        { property: "og:site_name", content: "Duck" },
+        { property: "og:url", content: "https://itsduck.com" },
+        { property: "og:image", content: "/og.png" },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:image:height", content: "630" },
+        { property: "og:image:type", content: "image/jpeg" },
 
-        // Default title/description
-        { name: "title", content: "Title" },
-        { name: "description", content: "Description" },
+        // Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "Duck" },
+        {
+          name: "twitter:description",
+          content:
+            "The first meme coin launchpad with contract-level rug protection. No trust required – just code that actually works.",
+        },
+        { name: "twitter:image", content: "/og.png" },
       ],
 
       link: [
         {
           rel: "icon",
-          type: "image/x-icon",
-          href: "/favicon.ico",
+          type: "image/png",
+          href: "/small-logo.webp",
         },
       ],
     },
