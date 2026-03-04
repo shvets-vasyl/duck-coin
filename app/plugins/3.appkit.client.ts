361b1983c4431966256554b957412e258f38639a
artbyk <artum128@gmail.com>
2026-03-04 10:00:10 +0100

    fix/walletconnect scroll

--- app/plugins/3.appkit.client.ts
+++ app/plugins/3.appkit.client.ts
@@ -22,17 +22,36 @@ export default defineNuxtPlugin((nuxtApp) => {
   const appKitState = useAppKitState()
   nuxtApp.vueApp.runWithContext(() => {
     setTimeout(() => {
+      let modalObserver: MutationObserver | null = null
+
+      const addPreventToModal = () => {
+        const modal = document.querySelector("w3m-modal")
+        if (modal) {
+          modal.setAttribute("data-lenis-prevent", "")
+          modal.querySelector("[part='container']")?.setAttribute("data-lenis-prevent", "")
+        }
+      }
+
       watch(
         () => appKitState.open,
         (isOpen) => {
           useLockScroll(isOpen)
           if (isOpen) {
-            const addPrevent = () => {
-              document.querySelector("w3m-modal")?.setAttribute("data-lenis-prevent", "")
-            }
-            addPrevent()
-            nextTick(addPrevent)
-            setTimeout(addPrevent, 200)
+            addPreventToModal()
+            nextTick(addPreventToModal)
+            setTimeout(addPreventToModal, 100)
+            setTimeout(addPreventToModal, 300)
+            setTimeout(addPreventToModal, 600)
+
+            modalObserver?.disconnect()
+            modalObserver = new MutationObserver(addPreventToModal)
+            modalObserver.observe(document.body, {
+              childList: true,
+              subtree: true,
+            })
+          } else {
+            modalObserver?.disconnect()
+            modalObserver = null
           }
         },
         { immediate: true }
